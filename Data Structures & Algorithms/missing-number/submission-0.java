02b5c812ab605043766b3271de89cb0d6173b05b
Hiyangshu Pal <hiyangshu.pal.75@gmail.com>
2026-08-30 21:01:31 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int len=nums.length;
+        int totSum=len*(len+1)/2;
+        int sum=0;
+        for(int i:nums) sum+=i;
+        return totSum-sum;
+    }
+}
